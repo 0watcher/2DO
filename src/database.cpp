140533ebcdcf75ba6abb32090f9d9ddffaf3cb95
patrykstruzek <patrykstruzek17@gmail.com>
2023-07-14 13:23:29 +0200

    Declarate db module

--- src/database.cpp
+++ src/database.cpp
@@ -1,1 +1,25 @@
+#include "database.hpp"
 
+
+
+std::vector<std::string> twodo::DBFileParser::read_line(const std::string& file_path, const std::string& key)
+{
+    return std::vector<std::string>();
+}
+
+void twodo::DBFileParser::write_line(const std::string& file_path, const std::string& line)
+{
+}
+
+void twodo::DBFileParser::delete_line(const std::string& file_path, const std::string& key)
+{
+}
+
+void twodo::DBFileParser::update_line(const std::string& file_path, const std::string& key, const std::string& new_line)
+{
+}
+
+bool twodo::DBFileParser::is_key_found(const std::string& file_path, const std::string& key)
+{
+    return false;
+}
---
+++ src/database.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace twodo
+{
+    class DBFileParser
+    {
+    public:
+        DBFileParser() = default;
+
+        std::vector<std::string> read_line(const std::string& file_path, const std::string& key);
+        void write_line(const std::string& file_path, const std::string& line);
+        void delete_line(const std::string& file_path, const std::string& key);
+        void update_line(const std::string& file_path, const std::string& key, const std::string& new_line);
+        bool is_key_found(const std::string& file_path, const std::string& key);
+    };
+}
