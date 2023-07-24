15cc5a89ab89c2e28f2b587a58970eb8d021d0cc
patrykstruzek <patrykstruzek17@gmail.com>
2023-07-24 18:55:09 +0200

    Implement test cases for login module

--- test/test.cpp
+++ test/test.cpp
@@ -5,18 +5,53 @@
 
 #include "result.hpp"
 #include "user.hpp"
+#include "utils.hpp"
 
 using namespace twodo;
 using namespace testing;
 
-
-TEST(LoginTest, PasswordTest)
+struct LoginTest : Test
 {
+    Login l{};
+};
 
+TEST_F(LoginTest, NicknameTest)
+{
+    std::string nicknm1 = "";
+    std::string nicknm2 = "nickname";
+
+    auto result1 = l.nickname(nicknm1);
+
+    EXPECT_EQ(result1.m_err, ErrorCode::incorrect_nickname);
+
+    auto result2 = l.nickname(nicknm2);
+
+    EXPECT_EQ(result2.m_err, ErrorCode::ok);
+    EXPECT_TRUE(result2.m_value.has_value());
 }
 
+TEST_F(LoginTest, PasswordTest)
+{
+    std::string passwd1 = "passwd";
+    std::string passwd2 = "password";
+    std::string passwd3 = "Pass_word1234";
 
-TEST(LoginTest, UserExistenceInDB)
+    auto result1 = l.password(passwd1);
+
+    EXPECT_EQ(result1.m_err, ErrorCode::incorrect_password);
+
+    auto result2 = l.password(passwd2);
+
+    EXPECT_EQ(result2.m_err, ErrorCode::incorrect_password);
+
+    auto result3 = l.password(passwd3);
+
+    EXPECT_EQ(result3.m_err, ErrorCode::ok);
+    EXPECT_TRUE(result3.m_value.has_value());
+    EXPECT_EQ(result3.m_value, hash(passwd3).m_value.value());
+}   
+
+TEST(UserDb, UserExistenceInDB)
 {
 
 }
