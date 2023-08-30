3f2a108a37c77feb2fa3bad2e01ecf719b3bd4ec
patrykstruzek <patrykstruzek17@gmail.com>
2023-08-30 13:34:24 +0200

    Add test for Db class

--- test/test.cpp
+++ test/test.cpp
@@ -1,18 +1,18 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-
 #include <string>
 
 #include "result.hpp"
 #include "user.hpp"
 #include "utils.hpp"
+#include "db.hpp"
 
 using namespace twodo;
 using namespace testing;
 
 struct LoginTest : Test
 {
-    Login l{};
+    Login cut{};
 };
 
 TEST_F(LoginTest, NicknameTest)
@@ -20,11 +20,11 @@ TEST_F(LoginTest, NicknameTest)
     std::string nicknm1 = "";
     std::string nicknm2 = "nickname";
 
-    auto result1 = l.nickname(nicknm1);
+    auto result1 = cut.nickname(nicknm1);
 
     EXPECT_EQ(result1.error(), LoginError::IncorrectNickname);
 
-    auto result2 = l.nickname(nicknm2);
+    auto result2 = cut.nickname(nicknm2);
 
     EXPECT_TRUE(result2);
 }
@@ -35,37 +35,54 @@ TEST_F(LoginTest, PasswordTest)
     std::string passwd2 = "password";
     std::string passwd3 = "Pass_word1234";
 
-    auto result1 = l.password(passwd1);
+    auto result1 = cut.password(passwd1);
 
     EXPECT_EQ(result1.error(), LoginError::IncorrectPassword);
 
-    auto result2 = l.password(passwd2);
+    auto result2 = cut.password(passwd2);
 
     EXPECT_EQ(result2.error(), LoginError::IncorrectPassword);
 
-    auto result3 = l.password(passwd3);
+    auto result3 = cut.password(passwd3);
 
     EXPECT_TRUE(result3);
     EXPECT_EQ(result3.value(), hash(passwd3).value());
-}   
-
-TEST(UserDb, UserExistenceInDB)
-{
-
 }
 
-TEST(UserManagerTest, AddUser)
+struct DbTest : Test
 {
+    Db cut{"C:/repos/2DO/db.db3"};
+};
 
-}
-
-TEST(UserManagerTest, DeleteUser)
+TEST_F(DbTest, ShouldStoreInDb)
 {
+    const std::string name = "patryk";
+    const std::string password = "patryk123";
 
+    auto ct_result = cut.create_table("users", {{"id", "INT"}, {"name", "TEXT"}, {"password", "TEXT"}});
+    if(!ct_result) {FAIL() << "CreateTableFailure";}
+    auto i_result = cut.insert_data("users", {{"id", "1"},
+                              {"name", name},
+                              {"password", password}});
+    if (!i_result) {FAIL() << "InsertTableFailure";}
+    auto s_result = cut.select_data("users", {"name", "password"}, {"id", "1"});
+    if(!s_result) {FAIL() << "SelectDataFailure";}
+
+    auto s_name = s_result.value()[0];
+    auto s_password = s_result.value()[1];
+
+    ASSERT_EQ(s_name, name);
+    ASSERT_EQ(s_password, password);
+
+    const std::string new_name = "dupa";
+
+    auto u_result = cut.update_data("users", {"name", new_name}, {"id", "1"});
+    if(!u_result) {FAIL() << "UpdateFailure";}
+
+    auto ns_result = cut.select_data("users", {"name", "password"}, {"id", "1"});
+    if(!ns_result) {FAIL() << "SelectDataFailure";}
+
+    auto updated_name = ns_result.value()[0];
+
+    ASSERT_EQ(updated_name, new_name);
 }
-
-TEST(UserManagerTest, ModifyUserData)
-{
-
-}
-
