3d541d0db450cfbc671c8160f24be57ce9c2b116
Patryk Strużek <patrykstruzek17@gmail.com>
2023-10-03 22:52:05 +0200

    Add compare operator for User class

--- src/lib/user.hpp
+++ src/lib/user.hpp
@@ -8,7 +8,6 @@
 #include "result.hpp"
 #include "utils.hpp"
 
-
 namespace twodo
 {
 enum class UserErr
@@ -60,6 +59,12 @@ class User
     {
     }
 
+    bool operator==(const User& other) const
+    {
+        return m_id == other.m_id && m_username == other.m_username && m_role == other.m_role &&
+               m_password == other.m_password;
+    }
+
     [[nodiscard]] int get_id() const { return m_id.value(); }
     [[nodiscard]] String get_username() const { return m_username; }
     [[nodiscard]] Role get_role() const { return m_role; }
@@ -71,7 +76,7 @@ class User
     void set_password(const String& passwd) { m_password = passwd; }
 
    private:
-    std::optional<int> m_id {};
+    std::optional<int> m_id {std::nullopt};
     String m_username {};
     Role m_role {};
     String m_password {};
@@ -87,11 +92,13 @@ class UserDb
     UserDb(UserDb&& other) = default;
     UserDb& operator=(UserDb&& other) = default;
 
-    [[nodiscard]] Result<User, UsrDbErr> get_user(const String& username) noexcept;
+    [[nodiscard]] Result<User, UsrDbErr> get_user(const String& username);
+    [[nodiscard]] Result<User, UsrDbErr> get_user(int id);
     [[nodiscard]] Result<int, UsrDbErr> get_user_id(const String& username);
     [[nodiscard]] bool is_empty();
     Result<None, UsrDbErr> add_user(const User& user);
     Result<None, UsrDbErr> delete_user(const String& username);
+    Result<None, UsrDbErr> delete_user(int id);
     Result<None, UsrDbErr> update_data(const User& user);
 
    private:
@@ -114,7 +121,6 @@ class RegisterManager
     UserDb m_udb;
     IUserInputHandler& m_ihandler;
     IDisplayer& m_idisplayer;
-
 };
 
 class AuthManager
